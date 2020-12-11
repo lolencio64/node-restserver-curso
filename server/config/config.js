25cb70667d688e81441f523f97d6fa3ba6c15184
Marco <marco.a.c@live.com>
2020-12-11 02:29:53 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //===============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//=================================
+//Entorno
+//===============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//=================================
+//Base de Datos
+//===============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://rusty:WgmTtzJUFP9Nvh4v@cluster0.uevuw.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
